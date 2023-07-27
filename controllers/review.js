47e253080b570449b1ba01a7cc0050ff2f4f662a
Boris Labianca <boris.labianca@hotmail.fr>
2023-07-27 21:50:33 +0200

    added verification logic to review controller

--- controllers/review.js
+++ controllers/review.js
@@ -1,1 +1,22 @@
-exports.addReview = (req, res) => {};
+const { isValidObjectId } = require("mongoose");
+const { sendError } = require("../utils/helper");
+const Movie = require("../models/Movie");
+const Review = require("../models/Review");
+
+exports.addReview = async (req, res) => {
+  const { movieId } = req.params;
+  const { content, rating } = req.body;
+  const userId = req.user._id;
+
+  if (!isValidObjectId(movieId)) return sendError(res, "Invalid movie.");
+
+  const movie = await Movie.findOne({ _id: movieId, status: "public" });
+  if (!movie) return sendError(res, "Movie not found.", 404);
+
+  const isAlreadyReviewed = await Review.findOne({
+    owner: userId,
+    parentMovie: movie._id,
+  });
+  if (isAlreadyReviewed)
+    return sendError(res, "Invalid request, user has already posted a review.");
+};
